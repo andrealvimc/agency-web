7bb85760b2acd1dedab3a5a3f989e5631489beb3
André Alvim <andre@isportistics.com>
2023-08-27 17:34:46 -0300

    feat: init login

---
+++ src/app/auth/login/components/user-auth-form.tsx
@@ -0,0 +1,88 @@
+"use client"
+
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+// import { Icons } from "@/components/icons"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Icons } from "@/components/ui/icon"
+// import { Button } from "@/registry/new-york/ui/button"
+// import { Input } from "@/registry/new-york/ui/input"
+// import { Label } from "@/registry/new-york/ui/label"
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+  const [isLoading, setIsLoading] = React.useState<boolean>(false)
+
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault()
+    setIsLoading(true)
+
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 3000)
+  }
+
+  return (
+    <div className={cn("grid gap-6", className)} {...props}>
+      <form onSubmit={onSubmit}>
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <Label className="" htmlFor="email">
+              Email
+            </Label>
+            <Input
+              id="email"
+              placeholder="joao@agenciaescalavel.com.br"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label className="" htmlFor="password">
+              Senha
+            </Label>
+            <Input
+              id="password"
+              placeholder="Senha"
+              type="password"
+              autoCapitalize="none"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+          </div>
+          <Button disabled={isLoading}>
+            {isLoading && (
+              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+            )}
+            Acessar
+          </Button>
+        </div>
+      </form>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            Ou continue com
+          </span>
+        </div>
+      </div>
+      <Button variant="outline" className="cursor-not-allowed" type="button" disabled={isLoading}>
+        {isLoading ? (
+          <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+        ) : (
+          <Icons.google className="mr-2 h-4 w-4" />
+        )}{" "}
+        Google
+      </Button>
+    </div>
+  )
+}
