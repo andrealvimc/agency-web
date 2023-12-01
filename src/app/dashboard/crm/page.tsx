5a3952a924e381f43d6a505d5a07a822e4ca1457
Andre Alvim <andre@alvim.net>
2023-12-01 20:17:26 -0300

    fix

--- src/app/dashboard/crm/page.tsx
+++ src/app/dashboard/crm/page.tsx
@@ -1,10 +1,18 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { KanbanBoard } from "@/components/crm/kanban-board";
+import { getServerSession } from "next-auth";
 
-export default function CRMPage() {
+export default async function  CRMPage() {
+
+ const session = await getServerSession(authOptions)
+
+ console.log(session?.user?.accountId)
+
   return (
     <div>
       <div className="flex items-center justify-between space-y-2">
         <h2 className="text-3xl font-bold tracking-tight">CRM</h2>
+        {session?.user?.accountId || "sem account"}
         <div className="flex items-center space-x-2">
           {/* <CreateAgency /> */}
           Funil de vendas
